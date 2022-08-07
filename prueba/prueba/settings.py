762325c67df733e412bcee003861c1bb8f356238
Isaias0927 <107389961+Isaias0927@users.noreply.github.com>
2022-08-07 16:51:21 -0500

    Agregando proyecto prueba

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'prueba.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ejemplo',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'ejemplo',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
